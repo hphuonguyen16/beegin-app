9efaccc06fe9a782b4f065bc2be2de3a698cf817
sumori <hoangthiphuonguyen1602@gmail.com>
2023-10-11 15:12:54 +0700

    save cookie and protected route

---
+++ src/config/urlConfig.ts
@@ -0,0 +1,7 @@
+const UrlConfig = {
+  user: {
+    login: `${process.env.NEXT_APP_BEEGIN_DOMAIN}/api/v1/users/login`
+  }
+}
+
+export default UrlConfig
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,35 +1,35 @@
-import { Session } from "next-auth";
-import { JWT } from "next-auth/jwt";
+import { Session } from 'next-auth'
+import { JWT } from 'next-auth/jwt'
 
-declare module "next-auth" {
-	interface Session {
-		id: string;
-		user: JWT;
-	}
+declare module 'next-auth' {
+  interface Session {
+    id: string
+    user: JWT
+  }
 
-	interface User {
-		_doc: {
-			_id: string;
-			avatar: string;
-			username: string;
-			email: string;
-			name: string;
-			surname: string;
-			quote: string;
-		};
-	}
+  interface User {
+    _doc: {
+      _id: string
+      name: string
+      email: string
+      phonenumber: string
+      role: string
+      preferences: any
+      createdAt: any
+    }
+  }
 }
 
-declare module "next-auth/jwt" {
-	interface JWT {
-		_doc: {
-			_id: string;
-			avatar: string;
-			username: string;
-			email: string;
-			name: string;
-			surname: string;
-			quote: string;
-		};
-	}
+declare module 'next-auth/jwt' {
+  interface JWT {
+    _doc: {
+      _id: string
+      name: string
+      email: string
+      phonenumber: string
+      role: string
+      preferences: any
+      createdAt: any
+    }
+  }
 }
