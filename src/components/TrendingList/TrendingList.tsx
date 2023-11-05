a6148fbc0c7cef69538715f06f2066b85d56d244
Haix1xx <haixinhtuoi123@gmail.com>
2023-11-05 23:07:38 +0700

    delete comment code

--- src/components/TrendingList/TrendingList.tsx
+++ src/components/TrendingList/TrendingList.tsx
@@ -1,30 +1,11 @@
 import { Grid, Typography, Box, Stack } from '@mui/material'
 import TrendingCard from './TrendingCard'
-import useAxiosPrivate from '@/hooks/useAxiosPrivate'
-import { useEffect, useState } from 'react'
-import UrlConfig from '@/config/urlConfig'
 import { TrendingHashtag } from '@/types/trendingHashtag'
-import { axiosPrivate } from '@/axios'
-import { InsertEmoticon } from '@mui/icons-material'
 
 interface HashTagListProps {
   trendingHashtags: TrendingHashtag[]
 }
 export default function TrendingList({ trendingHashtags }: HashTagListProps) {
-  // const [trendingHashtags, setTrendingHashtags] = useState<TrendingHashtag[]>([])
-  // const axios = useAxiosPrivate()
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.get(UrlConfig.trending.getTrendingHashtags)
-  //       setTrendingHashtags(response.data.data)
-  //     } catch (error) {}
-  //   }
-  //   fetchData()
-  //   console.log(trendingHashtags)
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [])
   return (
     <Grid
       sx={{
