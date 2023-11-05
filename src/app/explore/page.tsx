a5eec3cafcf1cda8e5d27aae8fc3ef03cc357ad7
Haix1xx <haixinhtuoi123@gmail.com>
2023-11-05 22:21:09 +0700

    trending hashtag

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,12 +1,14 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Grid, Typography, Box, Stack } from '@mui/material'
 
 import TrendingCard from '@/components/TrendingList/TrendingCard'
 import SuggestFollow from '@/components/SuggestFollow/SuggestFollow'
 import TrendingList from '@/components/TrendingList/TrendingList'
 import PostLayout from '@/layouts/PostLayout'
-
+import useAxiosPrivate from '@/hooks/useAxiosPrivate'
+import UrlConfig from '@/config/urlConfig'
+import { TrendingHashtag } from '@/types/trendingHashtag'
 //temp data
 const trendingItems = [
   {
@@ -29,10 +31,22 @@ const trendingItems = [
   }
 ]
 export default function Page() {
-  console.log('render page')
+  const [trendingHashtags, setTrendingHashtags] = useState<TrendingHashtag[]>([])
+  const axios = useAxiosPrivate()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(UrlConfig.trending.getTrendingHashtags)
+        setTrendingHashtags(response.data.data)
+      } catch (error) {}
+    }
+    fetchData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
     <PostLayout>
-      <TrendingList />
+      <TrendingList trendingHashtags={trendingHashtags} />
     </PostLayout>
   )
 }
