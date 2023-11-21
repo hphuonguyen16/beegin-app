b5b4e95a121698db0ca7c85d847f8e3acc063f6c
sumori <hoangthiphuonguyen1602@gmail.com>
2023-11-21 22:14:54 +0700

    change domain url

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true
   },
   env: {
-    NEXT_APP_BEEGIN_DOMAIN: 'http://localhost:8000',
+    NEXT_APP_BEEGIN_DOMAIN: 'https://beeginapp.onrender.com',
     NEXTAUTH_URL: 'http://localhost:3000'
   },
   images: {
