c946dfb685878afac532be69283eceb1154ed6fd
Haix1xx <haixinhtuoi123@gmail.com>
2023-12-22 11:55:57 +0700

    add isfollowing in user list

--- src/components/ListFollowing/ListFollowCard.tsx
+++ src/components/ListFollowing/ListFollowCard.tsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import { Card, CardHeader, Avatar, Button, Box } from '@mui/material'
+import { useAuth } from '@/context/AuthContext'
+import UrlConfig from '@/config/urlConfig'
+import { axiosPrivate } from '@/axios'
 
 export default function ListFollowCard(props: any) {
-  console.log(props)
-  const { name, username, avatar } = props.user
-  const [follow, setFollow] = React.useState(false)
+  const { name, username, avatar, isFollowing, _id } = props.user
+  const { user } = useAuth()
+  const [follow, setFollow] = React.useState(isFollowing)
+  const isUser = user?._id === _id
   const handleFollow = () => {
+    if (follow) {
+      unfollow(_id)
+    } else {
+      followOtherUser(_id)
+    }
     setFollow((prev: boolean) => !prev)
   }
+
+  const followOtherUser = async (id: any) => {
+    try {
+      const url = UrlConfig.me.followingOtherUser
+      await axiosPrivate.post(url, { id: id })
+    } catch (err) {}
+  }
+
+  const unfollow = async (id: any) => {
+    try {
+      const url = UrlConfig.me.unfollow.replace(':id', id)
+      await axiosPrivate.delete(url)
+    } catch (err) {}
+  }
+
   return (
     <Box sx={{ position: 'relative', padding: '0 !important', width: '100%' }}>
       <CardHeader
@@ -19,6 +43,7 @@ export default function ListFollowCard(props: any) {
           <Button
             onClick={handleFollow}
             variant={follow ? 'outlined' : 'contained'}
+            hidden={isUser}
             sx={{
               padding: '10px 20px',
               width: '100px',
